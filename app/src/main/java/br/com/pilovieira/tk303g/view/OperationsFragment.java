7583b7b3f7b60f8b613bf3de674d9ee0e41c8eea
Luiz Paulo Vieira <pilovieira@gmail.com>
2017-03-17 00:31:03 -0300

    refactor operations

--- app/src/main/java/br/com/pilovieira/tk303g/view/OperationsFragment.java
+++ app/src/main/java/br/com/pilovieira/tk303g/view/OperationsFragment.java
@@ -1,7 +1,14 @@
 package br.com.pilovieira.tk303g.view;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,19 +16,55 @@ import android.view.ViewGroup;
 
 import br.com.pilovieira.tk303g.R;
 import br.com.pilovieira.tk303g.business.TK303GCommands;
-import br.com.pilovieira.tk303g.business.ListenerProvider;
+import br.com.pilovieira.tk303g.comm.SMSEmitter;
+import br.com.pilovieira.tk303g.persist.Prefs;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class OperationsFragment extends Fragment {
+
+    private TK303GCommands commands;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        commands = new TK303GCommands(getContext());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_operations, container, false);
-
-        ListenerProvider.locationListener(view.findViewById(R.id.btn_get_location));
-        ListenerProvider.lock(view.findViewById(R.id.btn_lock_vehicle));
-        ListenerProvider.unlock(view.findViewById(R.id.btn_unlock_vehicle));
-
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick(R.id.btnGetLocation)
+    public void locationAction(View view) {
+        Prefs prefs = new Prefs(getContext());
+        String number = prefs.getTrackerNumber();
+        if (number.isEmpty()) {
+            Snackbar.make(view, R.string.msg_configure_tracker_number, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(commands.getLocation()));
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(view, R.string.please_add_call_permission, Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.btnLockVehicle)
+    public void lockAction() {
+        new SMSEmitter(getContext()).emit(getString(R.string.lock_vehicle), commands.lockVehicle());
+    }
+
+    @OnClick(R.id.btnUnlockVehicle)
+    public void unlockAction() {
+        new SMSEmitter(getContext()).emit(getString(R.string.unlock_vehicle), commands.unlockVehicle());
     }
 
 }
