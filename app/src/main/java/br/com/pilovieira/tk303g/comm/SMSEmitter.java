ab2f5f13b6899e6a6f51e6c417de0eb51a725181
Pilo Vieira <pilovieira@gmail.com>
2020-10-07 10:09:07 -0300

    remove SEND_SMS

--- app/src/main/java/br/com/pilovieira/tk303g/comm/SMSEmitter.java
+++ app/src/main/java/br/com/pilovieira/tk303g/comm/SMSEmitter.java
@@ -1,6 +1,8 @@
 package br.com.pilovieira.tk303g.comm;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 
@@ -35,15 +37,16 @@ public class SMSEmitter {
         if (prefs.getPassword().isEmpty())
             throw new RuntimeException(context.getString(R.string.msg_configure_password));
 
-        SmsManager.getDefault().sendTextMessage(prefs.getTrackerNumber(), null, message, null, null);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse("smsto:" + prefs.getTrackerNumber()));  // This ensures only SMS apps respond
+        intent.putExtra("sms_body", message);
+        context.startActivity(intent);
 	}
 
     private void log(String commandName, String command) {
         String log = context.getString(R.string.message_command_confirmation, commandName, command.replace(prefs.getPassword(), "******"));
         new ServerLogManager(context).log(LogType.COMMAND, log);
-
-        log = context.getString(R.string.message_confirmation, commandName);
-        Toast.makeText(context, log, Toast.LENGTH_SHORT).show();
     }
 
 }
